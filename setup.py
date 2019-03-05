4f8382c88f3eac7b725b5571f99ee3d7d47ee43d
Raphael Schubert <rfswdp@gmail.com>
2019-03-04 23:20:32 -0300

    updated manifest and package folder structure

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup
 
 setup(
     name="masonite-api-pagination",
-    version='0.0.1',
+    author="Raphael Schubert",
+    author_email="rfswdp@gmail.com",
+    description="Simple yet powerful masonite-api pagination",
+    url="https://github.com/rfschubert/masonite-api-pagination",
+    version='0.0.2',
     packages=['masonite-api-pagination'],
     install_requires=[
         'masonite',
+        'masonite-api'
     ],
     include_package_data=True,
 )

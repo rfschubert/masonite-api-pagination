3ba3971f964e35ef8f79d865a4817268246ffccf
Raphael Schubert <rfswdp@gmail.com>
2019-03-05 02:49:03 -0300

    version up to fix package colision with masonite-api

--- masonite_api_pagination/resources/PaginatedResource.py
+++ masonite_api_pagination/resources/PaginatedResource.py
@@ -9,9 +9,9 @@ from masonite_api_pagination.resources.FiltersHelper import FiltersHelper
 class PaginatedResource(Resource):
 
     def index(self):
-        return self.filter(query=None)
+        return self.paginate(query=None)
 
-    def filter(self, query=None):
+    def package_filter(self, query=None):
         if query is None:
             model = self.model
         else:
@@ -25,11 +25,15 @@ class PaginatedResource(Resource):
                 else:
                     model = model.where(res[0], res[2])
 
-        return self.paginate(model)
+        return model
 
     def paginate(self, query=None):
+
         if query is None:
             query = self.model
+
+        query = self.package_filter(query)
+
 
         max_rows = self.model._per_page
         current_page = None if self.request.input('page') is False else self.request.input('page')

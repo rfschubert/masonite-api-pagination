06233de1f40fa86c980ffb0e4eb318f5ea134dc1
Raphael Schubert <rfswdp@gmail.com>
2019-03-04 23:32:26 -0300

    updated package version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="masonite_api_pagination",
+    name="masonite-api-pagination",
     author="Raphael Schubert",
     author_email="rfswdp@gmail.com",
     description="Simple yet powerful masonite-api pagination",
     url="https://github.com/rfschubert/masonite_api_pagination",
-    version='0.0.5',
+    version='0.0.4',
     packages=[
         'masonite_api_pagination'
     ],

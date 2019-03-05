46672aad66dc126e92ed863b6b6986eb41038f17
Raphael Schubert <rfswdp@gmail.com>
2019-03-05 00:24:12 -0300

    added way to customize the query

--- masonite_api_pagination/resources/PaginatedResource.py
+++ masonite_api_pagination/resources/PaginatedResource.py
@@ -7,9 +7,15 @@ from api.resources import Resource
 class PaginatedResource(Resource):
 
     def index(self):
+        return self.paginate()
+
+    def paginate(self, query=None):
+        if query is None:
+            query = self.model
+
         max_rows = self.model._per_page
         current_page = None if self.request.input('page') is False else self.request.input('page')
-        results = self.model.paginate(max_rows, 1 if current_page is None else current_page)
+        results = query.paginate(max_rows, 1 if current_page is None else current_page)
         total_rows_count = results.total
         max_pages = math.ceil(total_rows_count / max_rows)
         previous = None
